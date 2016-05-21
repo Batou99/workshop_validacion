cf7202b279f8627d45617a0d4c508386ff847948
Batou99 <lorenzo.lopez@intec.es>
2016-05-21 09:45:18 +0000

    Ex1 completo

--- src/Ex1.hs
+++ src/Ex1.hs
@@ -2,7 +2,15 @@ module Ex1 where
 
 import Prelude as P
 import System.Environment as SE
+import Control.Monad (liftM)
+
+readLines :: [String] -> [Int]
+readLines = map P.read
 
 main = do
-  args <- SE.getArgs
-  mapM_ P.putStrLn args
+  -- getArgs :: IO [String]
+  filepath : xs <- SE.getArgs
+  -- readFile :: IO String
+  fileContents <- P.readFile filepath
+  let allLines = lines fileContents
+  print . sum $ readLines allLines
